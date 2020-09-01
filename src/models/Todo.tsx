ee6f277b6726f8bdd85fa9ebe4555f0120f92363
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-01 09:04:15 -0500

    update Todo model with category

--- src/models/Todo.tsx
+++ src/models/Todo.tsx
@@ -1,12 +1,14 @@
 
 export default class Todo {
     private text: string;
+    private category: string;
     private inEdit: boolean;
     private completed: boolean;
     private id: string;
 
-    constructor(text: string, inEdit: boolean, completed: boolean, id: string) {
+    constructor(text: string, category: string, inEdit: boolean, completed: boolean, id: string) {
         this.text = text;
+        this.category = category;
         this.inEdit = inEdit;
         this.completed = completed;
         this.id = id;
@@ -14,6 +16,10 @@ export default class Todo {
 
     setText (text: string) {
         this.text = text;
+    }
+
+    setCategory (category: string) {
+        this.category = category;
     }
 
     setInEdit (inEdit: boolean) {
@@ -26,6 +32,10 @@ export default class Todo {
 
     getText(): string {
         return this.text;
+    }
+
+    getCategory() {
+        return this.category;
     }
 
     getInEdit(): boolean {
