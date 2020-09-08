dc769af1ba4eb751e72d1a817175e75305bb132e
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-08 14:17:06 -0500

    dispatch action to update category state

---
+++ src/components/SortButtons/SortButtons.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+class SortButtons extends React.Component<any, any> {
+
+    getCategory(event: any) {
+        this.props.sortCategory(event.target.value);
+    }
+
+    render() {
+        return (
+            <div className="sort-buttons">
+                <button className="button" value="all" onClick={(event) => this.getCategory(event)}>All</button>
+                <button className="button" value="general" onClick={(event) => this.getCategory(event)}>General</button>
+                <button className="button" value="grocery" onClick={(event) => this.getCategory(event)}>Grocery</button>
+                <button className="button" value="work" onClick={(event) => this.getCategory(event)}>Work</button>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = (dispatch:any) => ({
+    sortCategory: (category: string) => dispatch({
+        type: 'SORT',
+        category: category
+    })
+});
+
+export default connect(null, mapDispatchToProps)(SortButtons);
