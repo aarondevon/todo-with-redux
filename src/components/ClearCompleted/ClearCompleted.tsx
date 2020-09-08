bb458109172c8e80404a99a55a3a6dfd9c234355
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-08 16:45:54 -0500

    add new component for clear completed button

---
+++ src/components/ClearCompleted/ClearCompleted.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ClearCompleted extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                <button className="button">Clear Completed</button>
+            </div>
+        )
+    }
+}
