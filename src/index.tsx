8ff03daea4669ac558c32f878f7b1b55eb441793
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-08 14:14:17 -0500

    move redux state to index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {combineReducers, createStore} from 'redux';
 import './index.css';
+import {addTodoReducer} from "./redux/reducers/add-todo-reducer";
+import {categoryReducer} from "./redux/reducers/category-reducer";
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION__?: any;
+    }
+}
+
+const store = createStore(
+    combineReducers({
+        addTodos: addTodoReducer,
+        category: categoryReducer
+    }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={store}>
     <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
