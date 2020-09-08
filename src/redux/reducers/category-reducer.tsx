db571998aea8303d3b88f4f00e64a9a1e88666c2
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-08 14:16:08 -0500

    add reducer for category

---
+++ src/redux/reducers/category-reducer.tsx
@@ -0,0 +1,16 @@
+const initialState = {category: 'all'};
+
+export const categoryReducer = (state = initialState, action: any) => {
+    switch (action.type) {
+        case 'SORT':
+            return {category: action.category}
+        // case 'GENERAL':
+        //     return {category: 'general'}
+        // case 'GROCERY':
+        //     return {category: 'grocery'}
+        // case 'WORK':
+        //     return {category: 'work'}
+        default:
+            return state;
+    }
+};
