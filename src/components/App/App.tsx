64b9eeed4c8da8f60fbbdb443adb404256a38509
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-09-08 16:46:59 -0500

    add sort button and clear completed components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,41 +1,25 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import {combineReducers, createStore} from 'redux';
 import AddTodo from '../AddTodo/AddTodo';
 import DisplayTodos from '../DisplayTodos/DisplayTodos'
 import '../../styles/styles.scss'
 import './App.scss';
-import {addTodoReducer} from "../../redux/reducers/add-todo-reducer";
+import SortButtons from "../SortButtons/SortButtons";
+import ClearCompleted from "../ClearCompleted/ClearCompleted";
 
-
-declare global {
-    interface Window {
-        __REDUX_DEVTOOLS_EXTENSION__?: any;
-    }
-}
-
-const store = createStore(
-    combineReducers({
-        addTodos: addTodoReducer
-    }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class App extends React.Component<any, any> {
 
     render() {
         return (
-            <Provider store={store}>
             <div id="app">
                 <h1>todos</h1>
                 <div className="todo-form">
                     <AddTodo />
-                    <DisplayTodos/>
+                    <SortButtons />
+                    <DisplayTodos />
+                    <ClearCompleted />
                 </div>
-                {/*<div className="todos">*/}
-                {/*    {this.getPrintTodos()}*/}
-                {/*</div>*/}
-
             </div>
-            </Provider>
         );
     }
 }
